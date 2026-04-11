980d21b57f61914ef33afd29fe0c2dbb6837ecb3
zaebee <zaebuntu@gmail.com>
2026-04-11 23:13:05 +0300

    Revert "Add S3 remote backend for terraform state"
    This reverts commit 66fedf70ccdb79df6eab0318631b837747e6b9e0.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -46,15 +46,3 @@ variable "asi_one_key" {
   type        = string
   sensitive   = true
 }
-
-variable "service_account_key" {
-  description = "Service account access key for state storage"
-  type        = string
-  sensitive   = true
-}
-
-variable "service_account_secret" {
-  description = "Service account secret key for state storage"
-  type        = string
-  sensitive   = true
-}
