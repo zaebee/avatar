c083ec8226f20401dd50b11243615285bd2f9c3c
zaebee <zaebuntu@gmail.com>
2026-04-11 23:52:48 +0300

    Add functions.invoker role

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,9 @@ provider "yandex" {
   service_account_key_file = "/app/authorized_key.json"
   folder_id = "b1gesh0suso3pvjrro56"
 }
+
+resource "yandex_resourcemanager_folder_iam_member" "functions_sa_invoker" {
+  folder_id = "b1gesh0suso3pvjrro56"
+  role     = "functions.invoker"
+  member   = "serviceAccount:ajeila5562o058l0q4eq"
+}
