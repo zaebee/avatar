4c54f5d36308323120df8ffcb154494439706c46
zaebee <zaebuntu@gmail.com>
2026-04-11 23:01:53 +0300

    fix: add SA and keys to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,10 +10,30 @@ terraform {
 
 provider "yandex" {
   service_account_key_file = "/app/authorized_key.json"
+  folder_id = "b1gesh0suso3pvjrro56"
+}
+
+resource "yandex_iam_service_account" "functions_sa" {
+  name        = "asi-one-functions"
+  description = "Service account for asi:one InstagramPoster functions"
+  folder_id   = "b1gesh0suso3pvjrro56"
+}
+
+resource "yandex_iam_service_account_static_access_key" "functions_sa_key" {
+  service_account_id = yandex_iam_service_account.functions_sa.id
+  description        = "Static access key for asi:one functions"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "functions_sa_editor" {
+  folder_id = "b1gesh0suso3pvjrro56"
+  role     = "editor"
+  member   = "serviceAccount:${yandex_iam_service_account.functions_sa.id}"
 }
 
 resource "yandex_storage_bucket" "photos" {
-  bucket = "asi-one-photos"
+  bucket     = "asi-one-photos"
+  access_key = yandex_iam_service_account_static_access_key.functions_sa_key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.functions_sa_key.secret_key
   
   versioning {
     enabled = false
@@ -21,8 +41,11 @@ resource "yandex_storage_bucket" "photos" {
 }
 
 resource "yandex_message_queue" "instagram_posts" {
-  name = "asi-one-instagram-posts"
+  name       = "asi-one-instagram-posts"
   region_id = "ru-central1"
+  
+  access_key = yandex_iam_service_account_static_access_key.functions_sa_key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.functions_sa_key.secret_key
   
   visibility_timeout_seconds = 300
   receive_wait_time_seconds = 0
@@ -35,6 +58,9 @@ resource "yandex_message_queue" "instagram_posts" {
 }
 
 resource "yandex_message_queue" "instagram_posts_dlq" {
-  name = "asi-one-instagram-posts-dlq"
+  name       = "asi-one-instagram-posts-dlq"
   region_id = "ru-central1"
+  
+  access_key = yandex_iam_service_account_static_access_key.functions_sa_key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.functions_sa_key.secret_key
 }
