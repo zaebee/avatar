f85da45a001157652746bdc30fe75663c57d0a08
zaebee <zaebuntu@gmail.com>
2026-04-11 17:39:14 +0300

    fix: add folder_id and cloud_id to provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,9 +9,9 @@ terraform {
 }
 
 provider "yandex" {
-  token   = var.token
-  folder_id = var.folder_id
-  cloud_id = var.cloud_id
+  token    = var.token
+  folder_id = "b1gesh0suso3pvjrro56"
+  cloud_id  = "b1gbmfuuno217asel61v"
 }
 
 resource "yandex_storage_bucket" "photos" {
