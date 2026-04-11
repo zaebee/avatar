21d91e8916e8f8304019b398edb3467eef5b3425
zaebee <zaebuntu@gmail.com>
2026-04-12 00:35:25 +0300

    Use data sources for triggers

--- terraform/functions.tf
+++ terraform/functions.tf
@@ -6,40 +6,10 @@ data "yandex_function" "asi_one_worker" {
   name = "asi-one-worker"
 }
 
-resource "yandex_function_trigger" "scheduler" {
-  name        = "asi-one-scheduler"
-  description = "Trigger every 5 minutes for IMAP poller"
-  
-  timer {
-    cron_expression = "*/5 * * * ? *"
-  }
-  
-  function {
-    id                = data.yandex_function.imap_poller.id
-    service_account_id = "ajeila5562o058l0q4eq"
-  }
-  
-  lifecycle {
-    ignore_changes = [function]
-  }
+data "yandex_function_trigger" "scheduler" {
+  name = "asi-one-scheduler"
 }
 
-resource "yandex_function_trigger" "mq_trigger" {
-  name        = "asi-one-mq-trigger"
-  description = "Trigger on new queue messages"
-  
-  message_queue {
-    queue_id             = "yrn:yc:ymq:ru-central1:b1gesh0suso3pvjrro56:asi-one-instagram-posts"
-    service_account_id  = "ajeila5562o058l0q4eq"
-    batch_cutoff         = 0
-  }
-  
-  function {
-    id                = data.yandex_function.asi_one_worker.id
-    service_account_id = "ajeila5562o058l0q4eq"
-  }
-  
-  lifecycle {
-    ignore_changes = [function, message_queue]
-  }
+data "yandex_function_trigger" "mq_trigger" {
+  name = "asi-one-mq-trigger"
 }
